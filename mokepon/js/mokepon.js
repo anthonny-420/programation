492048a57a9ee557ae8ec6ba32d3b3da1f4258bc
anthonny-420 <2808anthonny@gmail.com>
2025-09-30 11:53:19 -0500

    I've created the 'js' folder; it contains all the information about JavaScript

---
+++ mokepon/js/mokepon.js
@@ -0,0 +1,5 @@
+function petplayerselection() {
+    alert("YOU'VE SELECTED YOUR PET")
+}
+let petPlayerButton = document.getElementById('pet-button')
+petPlayerButton.addEventListener('click', petplayerselection)
