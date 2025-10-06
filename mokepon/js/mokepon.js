6aac70d45c8119147a4e1b52999473a0d2bc9fd3
anthonny-420 <2808anthonny@gmail.com>
2025-10-06 12:47:11 -0500

    I made the text change when the player selects a pet

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -1,5 +1,26 @@
-function petplayerselection() {
-    alert("YOU'VE SELECTED YOUR PET")
+function gameStart() {
+    let petPlayerButton = document.getElementById('pet-button');
+    petPlayerButton.addEventListener('click', petPlayerSelection);
 }
-let petPlayerButton = document.getElementById('pet-button')
-petPlayerButton.addEventListener('click', petplayerselection)
+
+function petPlayerSelection() {
+    let inputHipodoge = document.getElementById('Hipodoge');
+    let inputCapipepo = document.getElementById('Capipepo');
+    let inputRatigueya = document.getElementById('Ratigueya');
+    let spanPetPlayer = document.getElementById('petPlayer');
+
+    if (inputHipodoge.checked) {
+        spanPetPlayer.innerHTML = 'Hipodoge'
+    }
+    else if (inputCapipepo.checked) {
+        spanPetPlayer.innerHTML = 'Capipepo'
+    }
+    else if (inputRatigueya.checked) {
+        spanPetPlayer.innerHTML = 'Ratigueya'
+    }
+    else {
+    alert("YOU MUST SELECT ONE PET");
+    }
+}
+
+window.addEventListener('load', gameStart)
